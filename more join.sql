db0e46d9a4c24f4ec1ebebbf5fddd30bdeeee6bd
ispirett <38801423+Ispirett@users.noreply.github.com>
2019-05-21 15:27:56 -0400

    Update more join.sql

--- more join.sql
+++ more join.sql
@@ -35,3 +35,74 @@ ON id = movieid
 WHERE actorid = (SELECT id  FROM actor 
                                 WHERE name = 'Harrison Ford' )
 
+9:
+SELECT DISTINCT title FROM movie JOIN 
+casting ON id = casting.movieid 
+WHERE actorid = (SELECT id FROM actor 
+                                WHERE name = 'Harrison Ford'
+                                AND ord != 1)
+ 10:    
+ SELECT title, name FROM movie AS m JOIN
+casting AS c ON m.id = c.movieid
+JOIN actor ON actorid = actor.id
+
+WHERE  yr = 1962  AND ord = 1
+
+11:
+SELECT yr,COUNT(title) FROM
+  movie JOIN casting ON movie.id=movieid
+         JOIN actor   ON actorid=actor.id
+where name='John Travolta'
+GROUP BY yr
+HAVING COUNT(title)=(SELECT MAX(c) FROM
+(SELECT yr,COUNT(title) AS c FROM
+   movie JOIN casting ON movie.id=movieid
+         JOIN actor   ON actorid=actor.id
+ where name='John Travolta'
+ GROUP BY yr) AS t
+)
+
+12:
+SELECT title, name 
+      FROM movie JOIN casting ON (movieid = movie.id
+                                                              AND ord = 1)
+                              JOIN actor ON  ( actorid = actor.id)
+WHERE movie.id IN(
+               SELECT movieid FROM casting
+                 WHERE actorid IN (
+                          SELECT id FROM actor 
+                          WHERE name='Julie Andrews'))
+                       
+13:
+  SELECT name FROM actor
+  JOIN casting ON
+            actorid = actor.id
+  JOIN movie ON 
+            movieid = movie.id
+WHERE ord = 1
+GROUP BY name
+HAVING COUNT
+                   (movieid) >= 30
+                  
+ 14:
+    SELECT title, COUNT(name) as actors FROM movie
+            JOIN casting ON
+                       ( movie.id = movieid )
+            JOIN actor ON
+                        (actorid = actor.id)
+WHERE yr = 1978 
+       GROUP BY title
+       ORDER BY actors DESC, title ASC
+                  
+  15:
+      SELECT  name FROM casting
+          JOIN actor ON actor.id = casting.actorid
+           WHERE movieid IN (
+           SELECT movieid FROM casting
+           WHERE actorid = (
+           SELECT id
+            FROM actor
+              WHERE name= 'Art Garfunkel'))
+               AND name != 'Art Garfunkel'            
+
+                                
